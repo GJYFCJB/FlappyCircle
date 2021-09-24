4fa36a7934ecde89848ccfd944966a3278cbbf4f
GJYFCJB <89375316+GJYFCJB@users.noreply.github.com>
2021-09-23 19:32:45 -0600

    Add files via upload
    make some changes on grass

--- FlappyCat/FlappyCat/Grass.cpp
+++ FlappyCat/FlappyCat/Grass.cpp
@@ -11,13 +11,20 @@
 Grass::Grass (int grassHeight, int scrWidth, int scrHeight, sf::Texture & grassTexture) {
     
     grassRec_.setTexture(grassTexture);
+    //grassTexture is a sf::grassTexture
+    
     grassRec_.setTextureRect(sf::IntRect(0, 0, scrWidth, grassHeight));
+    //setTextureRect: Shapes can also be textured, just like sprites. To specify a part of the texture to be mapped to the shape, use the setTextureRect function.
+    
     grassRec_.setPosition(0, scrHeight - grassHeight);
+    //set the position of the grass
+    
     
 }
 
 void Grass::draw (sf::RenderWindow & window) {
     
     window.draw(grassRec_);
+    // draw the grass on the screen 
     
 }
